edd63fcf074f2c18998cc71be8aba98dbabe4dce
paulino1401 <paulino1400@gmail.com>
2020-05-17 20:30:54 -0500

    agreagacion de monos

--- src/puente/Puente.java
+++ src/puente/Puente.java
@@ -9,41 +9,54 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.lang.Thread;
 
 public class Puente extends Application implements EventHandler {
-    public Image azul = new Image("Image/mono_azul.png");
     public ImageView[] imgazul = new ImageView[4];
+    public ImageView[] imgrojo = new ImageView[4];
     public ImageView imgpuente = new ImageView("Image/puente_col.png");
     private Scene escena;
     private Button btnazul,btnrojo;
     private Label lpersona;
-    public GridPane gridPane;
+    public GridPane gridPane,gridPane2;
     public Thread pue;
+    private VBox vBox;
 
     @Override
     public void start(Stage primaryStage) {
         btnazul = new Button(" AZUL ");
         btnrojo = new Button(" ROJO ");
-        lpersona = new Label("Con que persona quiere empezar:  ");
-
-        btnazul.setOnAction(event -> {
-            pue = new hiloses(gridPane);
-            pue.start();
-          /*  for (int i = 0; i <4 ; i++) {
-                imgazul[i] = new ImageView(azul);
-                gridPane.add(imgazul[i],i,1);
-            }*/
-        });
-
+        lpersona = new Label("Con que persona quiere empezar:    ");
+        vBox = new VBox(4);
         gridPane = new GridPane();
+        gridPane2 = new GridPane();
+
         gridPane.add(lpersona,0,0);
         gridPane.add(btnazul,1,0);
         gridPane.add(btnrojo,2,0);
-        gridPane.add(imgpuente,4,1);
+        gridPane2.add(imgpuente,4,0);
+        for (int i = 0; i <4 ; i++) {
+            imgazul[i] = new ImageView();
+            gridPane2.add(imgazul[i],i,0);
+        }
+        for (int i = 0; i <4 ; i++) {
+            imgrojo[i] = new ImageView();
+            gridPane2.add(imgrojo[i],i+5,0);
+        }
+        btnazul.setOnAction(event -> {
+            pue = new hiloses(imgazul,imgrojo,0);
+            try{
+                pue.start();
+            }catch(Exception e){}
 
-        escena = new Scene(gridPane,900,600);
+        });
+        btnrojo.setOnAction(event -> {
+
+        });
+        vBox.getChildren().addAll(gridPane,gridPane2);
+        escena = new Scene(vBox,900,600);
         primaryStage.setTitle("Puente");
         primaryStage.setScene(escena);
         primaryStage.show();
--- src/puente/hiloses.java
+++ src/puente/hiloses.java
@@ -5,20 +5,76 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class hiloses extends  Thread{
-    GridPane gridPane;
-    ImageView[] imgazul = new ImageView[4];
+    public ImageView[] imgazul;
+    public ImageView[] imgrojo;
+    int opc;
+    int i=0;
+    int j=0;
+    int cont=0;
+    int monazul=0;
+    int monrojo=0;
     public Image azul = new Image("Image/mono_azul.png");
-    public hiloses(GridPane gridPane){
-        this.gridPane = gridPane;
+    public Image rojo = new Image("Image/mono_rojo.png");
+    public hiloses(ImageView[] imgazul,ImageView[] imgrojo,int opc){
+        this.imgazul=imgazul; this.imgrojo=imgrojo; this.opc = opc;
     }
 
     @Override
     public void run() {
         super.run();
-        for (int i = 0; i <4 ; i++) {
-            imgazul[i] = new ImageView(azul);
-            gridPane.add(imgazul[i],i,1);
+        try {
+            switch (opc){
+                case 0:
+                    LibereAzul();
+                    monazul++;
+                    while(monazul<4||monrojo<4){
+                        int x =(int)(Math.random()*7000+1000);
+                        int opc= (int)(Math.random()*2);
+                        System.out.println("MonA: "+monazul+"MonR: "+monrojo+" Tiempo: "+x+"  opc: "+opc+"  i: "+i+"  j: "+j);
+                        sleep(x);
+                        switch (opc){
+                            case 0:
+                                if(i<4){
+                                    LibereAzul();
+                                    monazul++;
+                                }else{
+                                    LibereRojo();
+                                    monrojo++;
+                                }
+                                break;
+                            case 1:
+                                if(j<4){
+                                    LibereRojo();
+                                    monrojo++;
+                                }else{
+                                    LibereAzul();
+                                    monazul++;
+                                }
+                                break;
+                        }
+                    }
+                    break;
+                case 1:
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+
+    }
+
+    public void LibereRojo() {
+        imgrojo[j].setImage(rojo);
+        j++;
+    }
+
+    private void Reserve() {
+        cont--;
+    }
+
+    public void LibereAzul() {
+        imgazul[i].setImage(azul);
+        i++;
     }
 }
